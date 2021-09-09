3df9b1d89f26398083024ce0d8805d0a6d25a65e
Daniel S. Billing <daniel.billing@intility.no>
2021-09-09 14:15:46 +0200

    Discord Socialite setup done

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\DiscordController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login/discord', [DiscordController::class, 'redirectToProvider'])->name('discord.login');
+Route::get('/login/discord/callback', [DiscordController::class, 'handleProviderCallback'])->name('discordLoginCallback');
