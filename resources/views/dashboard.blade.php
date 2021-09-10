566a7c3b4bae905b6c4ba1ed74c357f90b5f1817
Daniel S. Billing <daniel.billing@intility.no>
2021-09-10 14:03:25 +0200

    Added some info to dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,9 +6,26 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-            <div class="overflow-hidden bg-white shadow-xl sm:rounded-lg">
-                <h1>hello world</h1>
+        <div class="grid w-full grid-cols-2 gap-4 p-8 mx-auto max-w-7xl">
+            <div class="p-8 overflow-hidden bg-white shadow-xl sm:rounded-lg">
+                <h1 class="py-2 text-2xl">Når? <span class="text-purple-500">22. Okt &mdash; 31. Okt 2021</span></h1>
+                <h1 class="py-2 text-2xl">Hvor? <span class="text-purple-500">Hjemme hos Daniel og Hanne<sup>&dagger;</sup></span></h1>
+                <h1 class="py-2 text-2xl">Annen info:</h1>
+                <p>Soveplasser:</p>
+                <ul class="ml-2 list-disc list-inside">
+                    <li>Kjellerstue: <span class="text-purple-500">Dobbelseng, sammenleggbar gjesteseng og luftmadrass</span></li>
+                    <li>Gjesterom: <span class="text-purple-500">Dobbelseng</span></li>
+                    <li>Kjellerrom: <span class="text-purple-500">Gjesteseng</span></li>
+                    <li>Kontor: <span class="text-purple-500">Plass til luftmadrass/sammenleggbar gjesteseng<sup>&dagger;&dagger;</sup></span></li>
+                    <li>Åpen loftstue: <span class="text-purple-500">Plass til flere luftmadrass/sammenleggbar gjesteseng<sup>&dagger;&dagger;</sup></span></li>
+                </ul>
+                <div class="mt-6">
+                    <p class="text-xs text-gray-500"><sup>&dagger;</sup> Send oss en PM for adresse</p>
+                    <p class="text-xs text-gray-500"><sup>&dagger;&dagger;</sup> Senger må bringes for disse rommene</p>
+                </div>
+            </div>
+            <div class="p-8 overflow-hidden bg-white shadow-xl sm:rounded-lg">
+                <h1 class="py-2 text-2xl">Jeg vil være med!</h1>
             </div>
         </div>
     </div>
