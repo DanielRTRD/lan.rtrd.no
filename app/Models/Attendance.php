59ad66d8a6fe80451ee1c43096c8fee904613b91
Daniel S. Billing <daniel.billing@intility.no>
2021-09-13 20:06:14 +0200

    Added vaccine field to attendance

--- app/Models/Attendance.php
+++ app/Models/Attendance.php
@@ -2,12 +2,14 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Attendance extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
@@ -22,6 +24,7 @@ class Attendance extends Model
         'pillow_douvet_cover',
         'bringing_bed',
         'comment',
+        'vaccine',
         'user_id',
     ];
 
