02fd0d24c67cee4054c6c347865f6fb8728d139c
Daniel S. Billing <daniel.billing@intility.no>
2021-09-14 16:44:14 +0200

    Update PermissionSeeder.php

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -23,7 +23,7 @@ class PermissionSeeder extends Seeder
 
         $r1->givePermissionTo($p1->name);
 
-        $user = User::first();
+        $user = User::find(1);
         if($user) {
             $user->assignRole($r1);
         }
