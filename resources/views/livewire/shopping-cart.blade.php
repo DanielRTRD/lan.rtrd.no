61aa57faea3b95415604dad1e100702cc83a3da2
Daniel S. Billing <daniel.billing@intility.no>
2021-09-24 21:02:35 +0200

    Update shopping-cart.blade.php

--- resources/views/livewire/shopping-cart.blade.php
+++ resources/views/livewire/shopping-cart.blade.php
@@ -4,7 +4,7 @@
             <tr class="p-4 font-bold uppercase border-b border-gray-500 whitespace-nowrap">
                 <th class="w-4/6 text-left">Navn</th>
                 <th class="w-1/6 text-left">Stk</th>
-                <th class="w-1/6 text-right">Pris</th>
+                <th class="w-1/6 text-right">Pris Stk</th>
             </tr>
         </thead>
         <tbody>
@@ -23,7 +23,7 @@
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
                             </svg>
                         </button>
-                        <button wire:click="remove({{ $item->id }})" class="ml-3 text-red-400 hover:text-red-600">
+                        <button wire:click="remove({{ $item->id }})" class="mt-6 ml-3 text-red-400 sm:mt-0 lg:mt-6 xl:mt-0 hover:text-red-600">
                             <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
                               </svg>
@@ -39,12 +39,18 @@
     <div class="py-4 font-bold uppercase">
         Totalt: {{ $this->total }} kr
     </div>
-    <div>
+    <div class="grid grid-cols-2 gap-4">
         <button class="px-2 py-1 text-red-100 transition-colors duration-150 bg-red-700 rounded-lg focus:shadow-outline hover:bg-red-800" wire:click="removeAll">
             <svg xmlns="http://www.w3.org/2000/svg" class="inline-block w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
             </svg>
-            Remove All
+            {{ __('Fjern alle') }}
+        </button>
+        <button class="px-2 py-1 text-green-100 transition-colors duration-150 bg-green-700 rounded-lg focus:shadow-outline hover:bg-green-800" wire:click="order">
+            <svg xmlns="http://www.w3.org/2000/svg" class="inline-block w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4" />
+              </svg>
+            {{ __('Bestill') }}
         </button>
     </div>
 </div>
