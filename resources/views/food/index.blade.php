2213ec8f030a124331d0c3c00c28d7ae0470fec5
Daniel S. Billing <daniel.billing@intility.no>
2021-09-24 21:35:33 +0200

    Added a alert

--- resources/views/food/index.blade.php
+++ resources/views/food/index.blade.php
@@ -10,6 +10,11 @@
             <div class="w-full px-1 my-1 md:w-full lg:my-4 lg:px-4 lg:w-1/2">
                 <div class="p-8 py-4 overflow-hidden text-gray-200 bg-gray-800 rounded shadow-lg md:shadow-xl sm:rounded-lg">
                     <h1 class="mb-4 text-2xl text-green-200">Meny</h1>
+                    <div class="px-4 py-3 my-2 text-sm leading-normal text-blue-700 bg-blue-100 rounded" role="alert">
+                        <p><svg xmlns="http://www.w3.org/2000/svg" class="inline-block w-4 h-4 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                        </svg>{{ __('Siste dagen for å legge inn bestilling på mat er :date! Vi bestiller opp ordre som er lagt inn og betalt dagen etter.', ['date' => \Carbon\Carbon::parse(env('FOOD_LAST_ORDER_DATE'))->toFormattedDateString()]) }}</p>
+                    </div>
                     @livewire('food-list')
                 </div>
             </div>
