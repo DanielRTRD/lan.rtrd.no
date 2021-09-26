ba0dd97907e46a3af9dc5206326a9fcea4ddebb3
Daniel S. Billing <daniel.billing@intility.no>
2021-09-26 11:46:48 +0200

    Discord webhook

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -83,6 +83,52 @@ class OrderController extends Controller
     {   
         $order = Order::find($id);
         if (Carbon::parse(env('FOOD_LAST_ORDER_DATE'))->isFuture() && !$order->paid && !$order->ordered) {
+            /*
+            *
+            * DISCORD WEBHOOK
+            *
+            */
+            if (env('DISCORD_ATTENDANCE_WEBHOOK_URL')) {
+                $webhookurl = env('DISCORD_ATTENDANCE_WEBHOOK_URL');
+                $json_data = json_encode([
+                    "content" => ($order->user->name ? $order->user->name : $order->user->username)." har slettet sin ordre for mat!", // Message
+                    "tts" => false, // Enable text-to-speech
+                    // Embeds Array
+                    "embeds" => [
+                        [
+                            "title" => $order->user->username, // Embed Title
+                            "type" => "rich", // Embed Type
+                            "description" => "Orderen var på ".$order->amount." kr", // Embed Description
+                            "url" => route('dashboard'), // URL of title link
+                            "timestamp" => Carbon::now()->toIso8601String(), // Timestamp of embed must be formatted as ISO8601
+                            "color" => hexdec("ff0000"), // Embed left border color in HEX
+                            // Footer
+                            "footer" => [
+                                "text" => $order->user->username, // Username
+                            ],
+                            // Author
+                            "author" => [
+                                "name" => 'Jeg har slettet min ordre for mat!',
+                            ],
+                        ]
+                    ]
+                
+                ], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE );
+                $ch = curl_init( $webhookurl );
+                curl_setopt( $ch, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
+                curl_setopt( $ch, CURLOPT_POST, 1);
+                curl_setopt( $ch, CURLOPT_POSTFIELDS, $json_data);
+                curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);
+                curl_setopt( $ch, CURLOPT_HEADER, 0);
+                curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
+                $response = curl_exec( $ch );
+                curl_close( $ch );
+            }
+            /*
+            *
+            * END DISCORD WEBHOOK
+            *
+        */
             $order->delete();
         }
         return redirect()->route('food')->withSuccess('Ordre slettet.');
--- app/Http/Livewire/ShoppingCart.php
+++ app/Http/Livewire/ShoppingCart.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Livewire;
 
+use Carbon\Carbon;
 use App\Models\Order;
 use Livewire\Component;
 use App\Models\OrderItem;
@@ -68,6 +69,52 @@ class ShoppingCart extends Component
             $orderItem->amount = $item->price;
             $orderItem->save();
         }
+        /*
+        *
+        * DISCORD WEBHOOK
+        *
+        */
+        if (env('DISCORD_ATTENDANCE_WEBHOOK_URL')) {
+            $webhookurl = env('DISCORD_ATTENDANCE_WEBHOOK_URL');
+            $json_data = json_encode([
+                "content" => ($order->user->name ? $order->user->name : $order->user->username)." har lagt inn en ordre for mat!", // Message
+                "tts" => false, // Enable text-to-speech
+                // Embeds Array
+                "embeds" => [
+                    [
+                        "title" => $order->user->username, // Embed Title
+                        "type" => "rich", // Embed Type
+                        "description" => "Orderen er på ".$order->amount." kr", // Embed Description
+                        "url" => route('dashboard'), // URL of title link
+                        "timestamp" => Carbon::now()->toIso8601String(), // Timestamp of embed must be formatted as ISO8601
+                        "color" => hexdec("198754"), // Embed left border color in HEX
+                        // Footer
+                        "footer" => [
+                            "text" => $order->user->username, // Username
+                        ],
+                        // Author
+                        "author" => [
+                            "name" => 'Jeg har lagt inn en ordre for mat!',
+                        ],
+                    ]
+                ]
+            
+            ], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE );
+            $ch = curl_init( $webhookurl );
+            curl_setopt( $ch, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
+            curl_setopt( $ch, CURLOPT_POST, 1);
+            curl_setopt( $ch, CURLOPT_POSTFIELDS, $json_data);
+            curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);
+            curl_setopt( $ch, CURLOPT_HEADER, 0);
+            curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
+            $response = curl_exec( $ch );
+            curl_close( $ch );
+        }
+        /*
+        *
+        * END DISCORD WEBHOOK
+        *
+        */
         $this->removeall();
         $this->emit('orderAdded');
     }
