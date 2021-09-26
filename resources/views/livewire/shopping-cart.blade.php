e2d1925273751f51c70c6e3372a8f6017d34756d
Daniel S. Billing <daniel.billing@intility.no>
2021-09-26 10:59:27 +0200

    Spelling

--- resources/views/livewire/shopping-cart.blade.php
+++ resources/views/livewire/shopping-cart.blade.php
@@ -4,7 +4,7 @@
             <div class="px-4 py-3 my-2 text-sm leading-normal text-green-700 bg-green-100 rounded" role="alert">
                 <p><svg xmlns="http://www.w3.org/2000/svg" class="inline-block w-4 h-4 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4" />
-                  </svg>{{ __('Ordre er betalt og bestillt!') }}</p>
+                  </svg>{{ __('Ordre er betalt og bestilt!') }}</p>
             </div>
         @elseif ($this->order->paid === 1 && $this->order->ordered === 0)
             <div class="px-4 py-3 my-2 text-sm leading-normal text-green-700 bg-green-100 rounded" role="alert">
