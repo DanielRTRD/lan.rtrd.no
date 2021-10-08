02d92122933ec59fe07d6d2a7e02afd6f072ef03
Daniel S. Billing <daniel.billing@intility.no>
2021-10-09 00:19:02 +0200

    Update shopping-cart.blade.php

--- resources/views/livewire/shopping-cart.blade.php
+++ resources/views/livewire/shopping-cart.blade.php
@@ -25,7 +25,7 @@
             <div class="flex items-center justify-between w-full mb-8">
                 <div class="order-2 w-1/12"></div>
                 <div class="z-20">
-                    <div class="flex items-center w-10 h-10 {{ $this->order->paid === 1 ? 'bg-green-900' : 'bg-gray-900' }} rounded-full">
+                    <div class="flex items-center w-10 h-10 {{ $this->order->paid ? 'bg-green-900' : 'bg-gray-900' }} rounded-full">
                         <h1 class="mx-auto font-semibold text-white">
                             <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 9V7a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2m2 4h10a2 2 0 002-2v-6a2 2 0 00-2-2H9a2 2 0 00-2 2v6a2 2 0 002 2zm7-5a2 2 0 11-4 0 2 2 0 014 0z" />
@@ -33,7 +33,7 @@
                         </h1>
                     </div>
                 </div>
-                <div class="order-1 w-10/12 px-6 py-4 ml-4 {{ $this->order->paid === 1 ? 'bg-green-900' : 'bg-gray-900' }} rounded-lg shadow-xl">
+                <div class="order-1 w-10/12 px-6 py-4 ml-4 {{ $this->order->paid ? 'bg-green-900' : 'bg-gray-900' }} rounded-lg shadow-xl">
                     <div class="flex flex-row">
                         <h3 class="text-xl font-bold text-gray-200">{{ __('Betaling bekreftet') }}</h3>
                     </div>
@@ -43,7 +43,7 @@
             <div class="flex items-center justify-between w-full mb-8">
                 <div class="order-2 w-1/12"></div>
                 <div class="z-20">
-                    <div class="flex items-center w-10 h-10 {{ $this->order->ordered === 1 ? 'bg-green-900' : 'bg-gray-900' }} rounded-full">
+                    <div class="flex items-center w-10 h-10 {{ $this->order->ordered ? 'bg-green-900' : 'bg-gray-900' }} rounded-full">
                         <h1 class="mx-auto font-semibold text-white">
                             <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path d="M9 17a2 2 0 11-4 0 2 2 0 014 0zM19 17a2 2 0 11-4 0 2 2 0 014 0z" />
@@ -52,7 +52,7 @@
                         </h1>
                     </div>
                 </div>
-                <div class="order-1 w-10/12 px-6 py-4 ml-4 {{ $this->order->ordered === 1 ? 'bg-green-900' : 'bg-gray-900' }} rounded-lg shadow-xl">
+                <div class="order-1 w-10/12 px-6 py-4 ml-4 {{ $this->order->ordered ? 'bg-green-900' : 'bg-gray-900' }} rounded-lg shadow-xl">
                     <div class="flex flex-row">
                         <h3 class="text-xl font-bold text-gray-200">{{ __('Ordre bestilt') }}</h3>
                     </div>
