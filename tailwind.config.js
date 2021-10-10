319922e73f8a166c23a4e3b66ea93fe26fe6770f
Daniel S. Billing <daniel.billing@intility.no>
2021-10-10 12:47:55 +0200

    Added print config to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,10 @@ module.exports = {
             fontFamily: {
                 sans: ['Nunito', ...defaultTheme.fontFamily.sans],
             },
+            screens: {
+                'print': {'raw': 'print'},
+                // => @media print { ... }
+            }
         },
     },
 
