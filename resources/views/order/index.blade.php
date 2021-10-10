9ad9e70d617ff3e4d7daddfdfc80941082fbfb16
Daniel S. Billing <daniel.billing@intility.no>
2021-10-10 12:57:57 +0200

    Added subtotal for all orders

--- resources/views/order/index.blade.php
+++ resources/views/order/index.blade.php
@@ -6,6 +6,7 @@
     </x-slot>
 
     <div class="container px-4 pb-12 mx-auto mt-12 md:px-12">
+        <p class="text-gray-200 print:text-black print:mt-10 print:border-b print:py-2">Subtotal alle ordre: <span class="text-green-400">{{ $orders->sum('amount') }} kr</span></p>
         <div class="flex flex-wrap -mx-1 lg:-mx-4">
             @foreach ($orders as $order)
                 @livewire('order-card', ['order' => $order])
