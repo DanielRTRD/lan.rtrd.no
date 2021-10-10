8669c36232435055e0309d6797ec634f9a800af8
Daniel S. Billing <daniel.billing@intility.no>
2021-10-10 12:48:11 +0200

    Hide menu and header from print

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,7 +26,7 @@
 
             <!-- Page Heading -->
             @if (isset($header))
-                <header class="bg-black shadow">
+                <header class="bg-black shadow print:hidden">
                     <div class="px-4 py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
                         {{ $header }}
                     </div>
